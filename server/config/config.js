a3f291ee7c99e28520739e5e64775b20762a8bc2
rojb <ruben.justiniano96@gmail.com>
2020-06-27 18:31:39 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://rojb:vAZmtOXHEQ69oorR@cluster0-qyfp8.gcp.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
